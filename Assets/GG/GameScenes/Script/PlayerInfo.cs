de64890d3f092c71ae55cd3357d9471d72e95d27
usernameIcanuse <90302406+usernameIcanuse@users.noreply.github.com>
2023-08-06 21:48:24 +0900

    파일 입출력 캐릭터 store 연결
    1. myroom 캐릭터, 로비 캐릭터도 연결 해보기
    2. store item 클래스 내가 만들어놔도 되나? 아니면 따로 만드는 사람 있었나?

---
+++ Assets/GG/GameScenes/Script/PlayerInfo.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerInfo
+{
+     public int Level;
+     public int Exp;
+     public int ExpMax;
+     public int Money;
+     
+     public int CurrCharacter;
+     public bool[] AvailableCharacter;
+
+     
+
+    public PlayerInfo()
+    {
+
+
+    }
+
+
+    public int Get_Level()
+    {
+        return Level;
+    }
+
+    public int Get_Exp()
+    {
+        return Exp;
+    }
+
+    public int Get_ExpMax()
+    {
+        return ExpMax;
+    }
+
+    public int Get_Money()
+    {
+        return Money;
+    }
+    public int Get_CurrCharacter()
+    {
+        return CurrCharacter;
+    }
+    public void Set_Exp(int iExp)
+    {
+        Exp += iExp;
+    }
+    public void Set_Money(int iMoney)
+    {
+        Money += iMoney;
+    }
+
+    public void Set_CurrCharacter(int iCurrIndex)
+    {
+        CurrCharacter = iCurrIndex;
+    }
+
+    public bool Is_Character_Available(int iIndex)
+    {
+        return AvailableCharacter[iIndex];
+    }
+
+    public void Set_Character_Available(int iIndex)
+    {
+        AvailableCharacter[iIndex] = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
