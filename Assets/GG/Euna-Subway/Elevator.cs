758099d12981121b3d46cf7014bf0e847d19e9cc
euWna <112548635+euWna@users.noreply.github.com>
2023-08-18 21:43:20 +0900

    필요 스크립트 추가

---
+++ Assets/GG/Euna-Subway/Elevator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    //게임 phase 관리
+    int phase; //현재 구간 숫자
+
+    //phase별 랜덤 지진 이벤트 관리
+
+    //
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
