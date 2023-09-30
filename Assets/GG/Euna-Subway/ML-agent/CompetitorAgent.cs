a6c9cd727f6f17f96ba6c1f250842b07c9f4437b
euWna <112548635+euWna@users.noreply.github.com>
2023-09-30 18:04:53 +0900

    9월 마지막 주 작업들

--- Assets/GG/Euna-Subway/ML-agent/CompetitorAgent.cs
+++ Assets/GG/Euna-Subway/ML-agent/CompetitorAgent.cs
@@ -8,14 +8,13 @@ using System;
 
 public class CompetitorAgent : Agent
 {
-    /// euna
     /// <summary>
     /// Variable : Status / Move / Environment / Avoid / Hide / Item 총 6부문별로 구분
     /// </summary>
 
    //1. Status : Agent 상태값
     public CompetitorAgent agent;
-    public Animator animator;
+    private Animator animator;
 
     // agent 시점 카메라
     public Camera agentCamera;
@@ -48,7 +47,7 @@ public class CompetitorAgent : Agent
     public const float runSpeed = walkSpeed * 1.5f;
 
     // 점프 스케일 & 중력
-    public float jumpForce = 250f;
+    public float jumpForce = 7f;
     public Vector3 customGravity = new Vector3(0, -19.62f, 0);
 
     //Agent가 의도적으로 움직임을 멈춘 상태인지 여부
@@ -59,6 +58,7 @@ public class CompetitorAgent : Agent
     private bool isRun;
     /// 점프 중인지 여부 <see cref="Player.m_bIsJump"/>
     private bool isJump;
+    public float _rotate = 80f;
 
 
    //3. Environment
@@ -68,7 +68,17 @@ public class CompetitorAgent : Agent
     //Agent의 훈련 시작 위치
     private Vector3 startingPoint;
 
+    //Delta_reward
+    private Vector3 _destination = new Vector3(70.6200027f, 5.32999992f, 160.669998f);
+    float _prevDistance;
+    float _nextDistance;
 
+    //Reward Shaping
+    float _totalDistance;
+
+    //Obs
+    public List<GameObject> _agents;
+    public List<float> cal_agents;
 
     public float Get_HP()
     {
@@ -83,38 +93,46 @@ public class CompetitorAgent : Agent
     {
         //m_PV.RPC("Damaging", RpcTarget.All, fDamage);
     }
+
+    /*
     public bool Is_Usable()
     {
         return staminaIsUsable;
     }
+    */
 
     public void Use_Stamina()
     {
-        if (currentStamina <= 0f)
+        if (currentStamina < 0f)
         {
+            //Debug.Log("stamina unusable");
             currentStamina = 0f;
             staminaIsUsable = false;
             isRun = false;
             return;
         }
-        Debug.Log("use stamina");
+            //Debug.Log("use stamina");
         currentStamina -= staminaRate * Time.deltaTime;
+            isRun = true;
+        
     }
 
     private void Recover_Stamina()
     {
+        isRun = false;
         if (currentStamina > maxStamina)
         {
             currentStamina = maxStamina;
             return;
         }
 
+        //Debug.Log("recover stamina");
         currentStamina += staminaRecover * Time.deltaTime;
-        Debug.Log("recover stamina");
 
         if (currentStamina > 10f)
         {
             staminaIsUsable = true;
+            //Debug.Log("stamina usable");
         }
     }
 
@@ -136,7 +154,7 @@ public class CompetitorAgent : Agent
 
     private void Update()
     {
-
+               
     }
 
     public override void Initialize()
@@ -148,17 +166,20 @@ public class CompetitorAgent : Agent
         Physics.gravity = customGravity;
 
         //If not training mode, no max step and keep playing
-        if (!trainingMode) MaxStep = 0;
+        if (!trainingMode) MaxStep = 0; 
     }
 
     public override void OnEpisodeBegin()
     {
+        //Debug.Log("설마여기?");
         if (trainingMode)
         {
-            Debug.Log("New Episode");
             //agent 스폰
             this.transform.position = startingPoint;
 
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            _prevDistance = Vector3.Distance(_destination, startingPoint);
+            _totalDistance = Vector3.Distance(_destination, startingPoint);
             //낙하물, 아이템 위치 등 리셋 (Hide, Item 개발시)
             //subwayArea.ResetMap();
         }
@@ -172,6 +193,28 @@ public class CompetitorAgent : Agent
         agentRb.angularVelocity = Vector3.zero;
     }
 
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("goal"))
+        {
+            AddReward(100f);
+            Debug.Log("Goal");
+            //EndEpisode();
+        }
+
+        if (col.CompareTag("Checkpoint"))
+        {
+            AddReward(10f);
+            Debug.Log("Get the reward");
+        }
+
+        if (col.CompareTag("Block"))
+        {
+            SetReward(-30f);
+            Debug.Log("Hitted the Block");
+            //EndEpisode();
+        }
+    }
 
     public void OnCollisionEnter(Collision collision)
     {
@@ -179,6 +222,7 @@ public class CompetitorAgent : Agent
         if (collision.gameObject.CompareTag("stair"))
         {
             agentRb.AddForce(transform.forward * currentSpeed * 0.4f, ForceMode.VelocityChange);
+            AddReward(5f);
         }
         if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("stair"))
         {
@@ -186,6 +230,10 @@ public class CompetitorAgent : Agent
             animator.SetBool("IsJump", isJump);
             isGround = true;
             animator.SetBool("IsGround", isGround);
+        }
+        if (collision.gameObject.CompareTag("stair"))
+        {
+            agentRb.AddForce(transform.forward * currentSpeed * 0.4f, ForceMode.VelocityChange);
         }
 
         //훈련 관련
@@ -195,20 +243,15 @@ public class CompetitorAgent : Agent
 
             if (col.CompareTag("Wall"))
             {
-                SetReward(-1f);
-                EndEpisode();
+                SetReward(-30f);
+                Debug.Log("Wall");
+                //EndEpisode();
             }
 
             if (col.CompareTag("AI"))
             {
                 AddReward(-0.5f);
             }
-
-            if (collision.collider.CompareTag("Goal"))
-            {
-                AddReward(5f);
-            }
-
             /*
             이후 Obstacle, Falling, Item 추가
             */
@@ -219,22 +262,47 @@ public class CompetitorAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         //Goal 지점까지의 거리 (DeltaReward)
-
+        sensor.AddObservation(_nextDistance); //도착지와의 거리 (변수명 재활용)
+        for (int i = 0; i < _agents.Count; i++)
+        {
+            sensor.AddObservation(cal_agents[i]);
+        }
         //
     }
 
     //Revised (9.24.)
     public override void OnActionReceived(ActionBuffers actions)
     {
+        
+        //Decision Requester 없애고 해야 하실 거에요
+        //agent.RequestDecision();
+        //Goal 지점까지의 거리 (DeltaReward)
+        _nextDistance = Vector3.Distance(transform.position, _destination);
+        //Debug.Log("staminaIsUsable : " + staminaIsUsable);
+        //Debug.Log("이전거리 : " + _prevDistance + "현재 거리 : " + _nextDistance);
+
+        //DeltaReward
+        /*if (_nextDistance < _prevDistance) { AddReward(0.01f); }
+        else { AddReward(-0.002f); }
+        _prevDistance = _nextDistance;*/
+
+        //GradualReward
+        AddReward(((_totalDistance -_nextDistance)/_totalDistance));
+        
+
+        //관측
+        _calDistance();
+
         //actionMove 호출할 때마다 패널티를 부여하여
         //action을 줄이도록 (즉 에피소드를 빠르게 클리어하도록) 유도
-        AddReward(-0.01f);
+        AddReward(-0.05f);
 
         var dir = Vector3.zero;
+        dir += transform.forward;
 
         //DiscreteAction[0] 앞으로 이동 여부 2
-        var actionForward = actions.DiscreteActions[0];
-        switch (actionForward)
+        
+        /*switch (actionForward)
         {
             //Heuristic 테스트 외에는 case 0 삭제 
             case 0:
@@ -245,50 +313,69 @@ public class CompetitorAgent : Agent
                 frozen = false;
                 dir += transform.forward;
                 break;
-        }
+        }*/
 
         //DiscreteAction[1] 이동 방향 (좌:0 우:1)
-        var actionDir = actions.DiscreteActions[1];
+        var actionDir = actions.DiscreteActions[0];
+
         switch (actionDir)
         {
             case 0:
+                //dir -= transform.right;
+                transform.Rotate(Vector3.up, Time.deltaTime * -_rotate);
                 break;
             case 1:
-                dir -= transform.right;
-                break;
-            case 2:
-                dir += transform.right;
+                //dir += transform.right;
+                transform.Rotate(Vector3.up, Time.deltaTime * _rotate);
                 break;
         }
 
-        //DiscreteAction[2] 달리기 여부 2
-        var actionRun = actions.DiscreteActions[2];
+        /*switch (actionDir)
+        {
+            case 0: break;
+            case 1:
+                //dir -= transform.right;
+                transform.Rotate(Vector3.up, Time.deltaTime * -_rotate);
+                break;
+            case 2:
+                //dir += transform.right;
+                transform.Rotate(Vector3.up, Time.deltaTime * _rotate);
+                break;
+        }*/
 
-        
+        //DiscreteAction[2] 달리기 여부 2
+        var actionRun = actions.DiscreteActions[1];
         switch (actionRun)
         {
             case 0:
-                isRun = false;
                 currentSpeed = walkSpeed;
-                Debug.Log("walk");
+                Recover_Stamina();
+                //Debug.Log("walk");
                 break;
             case 1:
-                if (!isRun && staminaIsUsable)
+                if (staminaIsUsable)
                 {
+                    animator.SetBool("IsSprint", isRun);
                     currentSpeed = runSpeed;
-                    isRun = true;
-                    Debug.Log("Run");
+                    Use_Stamina();
+                    //Debug.Log("Run");
+                }
+                else
+                {
+                    currentSpeed = walkSpeed;
+                    //Debug.Log("Run unable");
                 }
                 break;
         }
 
+        //Debug.Log("isRun : " + isRun);
         //이동 (MovePosition 적용)
         agentRb.MovePosition(transform.position + dir * currentSpeed * Time.deltaTime);
 
         //스태미나
-        if (isRun) Use_Stamina();
-        else Recover_Stamina();
-        Debug.Log("stamina : " + currentStamina);
+        //if (isRun) Use_Stamina();
+        //else Recover_Stamina();
+        //Debug.Log("stamina : " + currentStamina);
 
         /*
         if (actionRun == 1 && !isRun && staminaIsUsable)
@@ -301,11 +388,14 @@ public class CompetitorAgent : Agent
 
 
         //DiscreteAction[3] 점프 여부 2
-        var actionJump = actions.DiscreteActions[3];
+        var actionJump = actions.DiscreteActions[2];
         if (actionJump == 1 && !isJump)
         {
             agentRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isJump = true;
+            animator.SetBool("isJump", isJump);
+            isGround = false;
+            animator.SetBool("isGround", isGround);
         }
 
         //애니메이터?? 다시 확인
@@ -344,6 +434,17 @@ public class CompetitorAgent : Agent
         if (Input.GetKeyDown(KeyCode.Space))
         {
             action[3] = 1;
+        }
+    }
+
+    void _calDistance()
+    {
+        cal_agents = new List<float>();
+        for (int i = 0; i < _agents.Count; i++)
+        {
+            float d;
+            d = Vector3.Distance(_agents[i].transform.position, transform.position);
+            cal_agents.Add(d);
         }
     }
 
--- Assets/GG/Euna-Subway/phase2/ItemArea.cs
+++ Assets/GG/Euna-Subway/phase2/ItemArea.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class ItemArea : MonoBehaviour
 {
