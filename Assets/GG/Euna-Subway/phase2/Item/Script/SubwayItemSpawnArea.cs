21afdec6d454b6b891e06ba0669f1619f5a67517
euWna <112548635+euWna@users.noreply.github.com>
2023-11-08 19:49:34 +0900

    로고 fbx 추가 & itemspawnarea 작성중

--- Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItemSpawnArea.cs
+++ Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItemSpawnArea.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 
 public class SubwayItemSpawnArea : MonoBehaviour
 {
-    public List<Transform> spawnPoints;
+    private Dictionary<Transform, GameObject> spawnPoints = new Dictionary<Transform, GameObject>();
     public GameObject itemOnField; //필드 아이템 프리팹
 
-    //아이템 랜덤 스폰
-    private void spawnItem ()
+    private void Awake()
     {
+        Transform[] spawnPoint = GetComponentsInChildren<Transform>();
+        foreach(Transform t in spawnPoint)
+        {
+            spawnPoints.Add(t);
+        }
+    }
 
+    private void Start()
+    {
+        //spawnItem();
+    }
+
+    //아이템 랜덤 스폰
+    private void spawnItem (int idx)
+    {
+        
     }
 
     //아이템 먹힌 자리 확인 후 랜덤 스폰 실행
-    private void random
+    private void checkBlank()
+    {
+
+    }
 }
