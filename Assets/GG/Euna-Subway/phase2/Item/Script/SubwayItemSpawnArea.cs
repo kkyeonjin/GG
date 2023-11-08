755c2a12f139918529dd175c873f7c8ccc2b34df
euWna <112548635+euWna@users.noreply.github.com>
2023-11-08 16:34:24 +0900

    필드 아이템 프리팹 + 스폰 로직 작성 중

---
+++ Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItemSpawnArea.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwayItemSpawnArea : MonoBehaviour
+{
+    public List<Transform> spawnPoints;
+    public GameObject itemOnField; //필드 아이템 프리팹
+
+    //아이템 랜덤 스폰
+    private void spawnItem ()
+    {
+
+    }
+
+    //아이템 먹힌 자리 확인 후 랜덤 스폰 실행
+    private void random
+}
