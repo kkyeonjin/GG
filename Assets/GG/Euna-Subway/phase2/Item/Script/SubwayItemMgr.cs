c2e4997f58881afec15780d542eb88f304ba0a38
박현진 <88366217+kkyeonjin@users.noreply.github.com>
2023-11-10 00:55:20 +0900

    지하철 아이템 스폰 구현

--- Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItemMgr.cs
+++ Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItemMgr.cs
@@ -3,26 +3,38 @@ using UnityEngine;
 
 public class SubwayItemMgr : MonoBehaviour
 {
-    public static SubwayItemMgr Instance = null;
+    public static SubwayItemMgr m_Instance = null;
     private void Awake()
     {
-        /*
         var duplicated = FindObjectsOfType<SubwayItemMgr>();
 
         if (duplicated.Length > 1)
-        {//이미 생성해서 플레이어 있음
+        {
             Destroy(this.gameObject);
         }
         else
-        {//처음 생성
-            if (null == Instance)
+        {
+            if (null == m_Instance)
             {
-                Instance = this;
+                m_Instance = this;
             }
         }
-        */
-        Instance = this;
     }
+
+    public static SubwayItemMgr Instance
+    {
+        get
+        {
+            if(null == m_Instance)
+            {
+                return null;
+            }
+            return m_Instance;
+        }
+    }
+
+    //필드 아이템 프리팹
+    public GameObject itemOnField; 
 
     //Field item 프리팹
     public List<SubwayItem> subItemList = new List<SubwayItem>();
@@ -33,14 +45,5 @@ public class SubwayItemMgr : MonoBehaviour
 
     //Grabbed 방해 아이템 프리팹
     public List<GameObject> GrabbableItems = new List<GameObject>(new GameObject[3]);
-
-    private void Update()
-    {
-        //게임 실행 중 아이템 스폰
-        //GameObject _item = Instantiate(subItemPrefab, subItemPos[i], Quaternion.identity);
-
-        // 아이템 종류 랜덤 결정
-        //_item.GetComponent<SubwayItems>().SetItem(subItemDB[Random.Range(0,n)]);
-    }
 
 }
