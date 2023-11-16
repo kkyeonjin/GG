6904843f9fdba4a640cd7a10ff154abc680e6cd6
euWna <112548635+euWna@users.noreply.github.com>
2023-11-16 11:44:10 +0900

    temp

--- Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItem.cs
+++ Assets/GG/Euna-Subway/phase2/Item/Script/SubwayItem.cs
@@ -81,6 +81,12 @@ public class SubwayItem : MonoBehaviour
                 Item_vanish();
             };
         }
+
+        else if (other.CompareTag("CompAI"))
+        {
+            Item_vanish();
+        }
+
     }
 
     public bool Get_isUsed() //Inventory.rearrange()에서 참조
